eb69a82db2de288f017731a3665ae51c9b345bc0
nirwan1265 <tandukar.nirwan@gmail.com>
2022-04-19 11:13:18 -0400

    Update

--- scripts/Shiny.kernel.R
+++ scripts/Shiny.kernel.R
@@ -1,51 +1,24 @@
-sorghum <- vroom::vroom("Data/data.csv")
-maize <- vroom::vroom("Data/data2.csv")
+sb <- vroom::vroom("Data/data.csv")
+zm <- vroom::vroom("Data/data2.csv")
 
 
 ui <- fluidPage(
-<<<<<<< HEAD
   selectInput("cropID", "Crop Type", 
               c("Maize" = "maize",
                 "Sorghum" = "sorghum")),
   tableOutput("crop_data")
-=======
-  selectInput("dataset", "Crop Type", 
-              c("Maize" = "maize",
-                "Sorghum" = "sorghum")),
-      sidebarLayout(
-      sidebarPanel(
-        conditionalPanel(
-          'input.dataset' == "maize"
-        ),
-        conditionalPanel(
-          'input.dataset' == "sorghum"
-        )
-      ),
-      mainPanel(
-        tabsetPanel(
-          id = 'dataset',
-          tabPanel("maize", DT::dataTableOutput("mytable1")),
-          tabPanel("sorghum", DT::dataTableOutput("mytable2"))
-        )
-      )
-    )
->>>>>>> ea23b57ee334de0574c3aaa20b44558afd437b6c
+    
 )
 server <- function(input, output, session){
-  # maize_data <- DT::renderDataTable({
-  #  DT::datatable(maize, options = list(orderClasses = TRUE))
-  # })
-  # sorghum_data <- DT::renderDataTable({
-  #  DT::datatable(sorghum, options = list(orderClasses = TRUE))
-  # })
-  maize_data <- data.frame(maize)
-  sorghum_data <- data.frame(sorghum)
-  
+
+  maize_data <- data.frame(zm)
+  sorghum_data <- data.frame(sb)
+
   datasetInput <- reactive({
-    if(input$cropID == "Maize"){
+    if(input$cropID == "maize"){
       dataset <- maize_data
     }
-    else if (input$cropID == "Sorghum"){
+    else if (input$cropID == "sorghum"){
       dataset <- sorghum_data
     }
     return(dataset)
@@ -60,6 +33,7 @@ server <- function(input, output, session){
 shinyApp(ui, server)
 
 
-maize_data <- DT::renderDataTable({
-  DT::datatable(maize, options = list(orderClasses = TRUE))
- })  
+
+
+
+
