bc0e04c77586cb682469a6f84031d3904dedb174
nirwan1265 <tandukar.nirwan@gmail.com>
2022-04-20 15:30:38 -0400

    update

--- scripts/app2.R
+++ scripts/app2.R
@@ -1,5 +1,8 @@
 library(shiny)
 
+sb <- vroom::vroom("Data/data.csv")
+zm <- vroom::vroom("Data/data2.csv")
+
 # Define UI for random distribution app ----
 ui <- fluidPage(
   
@@ -23,14 +26,16 @@ ui <- fluidPage(
       # Input: Slider for the Phosphorus and Altitude ----
       sliderInput("n",
                   "Phosphorus content:",
-                  value = 1,
+                  value = c(40,60),
                   min = 1,
-                  max = 100),
+                  max = 100,
+                  ),
       sliderInput("n.a",
                 "Altitude:",
-                value = 1,
+                value = c(2000,3000),
                 min = 1,
-                max = 5000)
+                max = 5000,
+                )
     
   ),
     
@@ -39,10 +44,11 @@ ui <- fluidPage(
       
       # Output: Tabset w/ plot, summary, and table ----
       tabsetPanel(type = "tabs",
+                  tabPanel("Table", tableOutput("table")),
                   tabPanel("Phosphorus Plot", plotOutput("p.plot")),
                   tabPanel("Altitude Plot", plotOutput("a.plot")),
-                  tabPanel("Summary", verbatimTextOutput("summary")),
-                  tabPanel("Table", tableOutput("table"))
+                  tabPanel("PCA", tableOutput("PCA")),
+                  tabPanel("Summary", verbatimTextOutput("summary"))
       )
       
     )
@@ -51,7 +57,7 @@ ui <- fluidPage(
 
 
 #Define server logic for random distribution app ----
-  server <- function(input, output) {
+  server <- function(input, output){
     
     # Reactive expression to generate the requested distribution ----
     # This is called whenever the inputs change. The output functions
@@ -63,9 +69,21 @@ ui <- fluidPage(
                      lnorm = rlnorm,
                      exp = rexp,
                      rnorm)
-      
+
       dist(input$n)
     })
+    
+    # data_active <- reactive({
+    #   # if user switches to internal data, switch in-app data
+    #   observeEvent(input$sample_or_real_button){
+    #     if(input$sample_or_real == "zm"){
+    #       data <- zm
+    #     } else {
+    #       data <- sb
+    #     }
+    #   }) 
+    
+    
     
     # Generate a plot of the data ----
     # Also uses the inputs to build the plot label. Note that the
@@ -87,10 +105,22 @@ ui <- fluidPage(
     })
     
     # Generate an HTML table view of the data ----
-    output$table <- renderTable({
-      d()
+    df <- eventReactive(input$dist, {
+        data_internal <- zm
+      } else {
+        data_internal <- sb
+      }
+    )
+    # output$table <- renderTable({
+    #   df()
+    # })
+    output$table <- DT::renderTable({
+      DT::datatable(data = df(), options = list(pageLength = 25),
+                    rownames = FALSE, class = 'display', escape = FALSE)
     })
-    
+    # output$mtable <- DT::renderDataTable({
+    #   DT::datatable(data = fully_filtered(), options = list(pageLength = 10),
+    #                 rownames = FALSE, class = 'display', escape = FALSE)
   }
 
 
